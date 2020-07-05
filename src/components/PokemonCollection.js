85b930e7d0e8db84c29d105ea43eefb2430a5aca
Joshua Mclean <joshmcleana@gmail.com>
2020-07-04 18:52:59 -0700

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,16 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+  
+  renderPokemon = () => {
+  // console.log(this.props.allPokemon)
+  return this.props.allPokemon.map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon} toggleImage={this.props.toggleImage}/>)
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+         {this.renderPokemon()}
       </Card.Group>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Search = props => {
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input onChange={props.handleSearch}className="prompt"/>
         <i className="search icon" />
       </div>
     </div>
