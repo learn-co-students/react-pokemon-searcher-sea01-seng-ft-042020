dce99aa3aba4c086a0f19c48bf36e0b42b4c2275
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-07 21:53:10 -0700

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -4,17 +4,76 @@ import PokemonForm from './PokemonForm'
 import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
+const API = 'http://localhost:3000/pokemon'
+
 class PokemonPage extends React.Component {
+  
+  state = {
+    pokemon: [],
+    searchPokemon: [],
+    newPokemon: {
+      name: "",
+      hp: "",
+      sprites: {
+        front: "",
+        back: ""
+      }
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchPokemon()
+  }
+
+  fetchPokemon () {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        pokemon: data,
+        searchPokemon: data
+      })
+    })
+  }
+  
+  handleChange = (e) => {
+   let newPokemonSearch = this.state.searchPokemon.filter(pokemon => 
+          pokemon.name.includes(e.target.value))
+      this.setState({ pokemon: newPokemonSearch})
+  }
+
+  handleNewPokemon = (e) => {
+    let caughtPokemon = this.state.newPokemon
+    this.setState({
+      newPokemon: {
+        ...caughtPokemon,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  handleSubmit = () => {
+    fetch(`${API}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state.newPokemon)
+    })
+    .then(response => response.json())
+    .then(data => this.fetchPokemon())
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm onHandleSubmit={this.handleSubmit} onHandleNewPokemon={this.handleNewPokemon} />
         <br />
-        <Search />
+        <Search onHandleChange={this.handleChange} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon} />
       </Container>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Search = props => {
+
+const onChange = (e) => {
+  e.preventDefault()
+  props.onHandleChange(e)
+}
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input className="prompt" onChange={(e) => onChange(e)} />
         <i className="search icon" />
       </div>
     </div>
